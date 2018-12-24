b52b7754bb937e86419b4e713082ab203f20e953
reachvedprakash <reachvedprakash@gmail.com>
2018-12-24 18:29:06 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
+from contacts.models import Contact
+
+# Create your views here.
+def register(request):
+    if(request.method == 'POST'):
+        # get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # Check for password
+        if password == password2 :
+            # Check user
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'That username is taken')
+                return redirect('register')
+            else :
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,'That email is taken')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username,password=password , email=email , first_name=first_name , last_name=last_name)
+                    #auth.login(request,user)
+                    #messages.sucess(request,"you are now logged in")
+                    user.save()
+                    messages.success(request,'you r now registered')
+                    return redirect('register')
+        else :
+            messages.error(request,'Password Doesnt match')
+            return redirect('register')
+    else : 
+        return render(request,'accounts/register.html')
+
+def login (request):
+    if(request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None :
+            auth.login(request,user)
+            messages.success(request,'you r login')
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Invalid credentials')
+            return redirect('login')
+    else : 
+        return render(request,'accounts/login.html')
+
+def logout(request):
+    if(request.method == 'POST'):
+        auth.logout(request)
+        messages.success(request,'you r now logged out')
+        return redirect('index')
+
+def dashboard(request):
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context={
+        'contacts' : user_contacts
+    }
+    return render(request,'accounts/dashboard.html',context)
---
+++ listings/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render , get_object_or_404
+from django.core.paginator import  EmptyPage, PageNotAnInteger ,Paginator
+from listings.choices import price_choices, bedroom_choices , state_choices
+from .models import Listing
+
+
+# Create your views here.
+
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+
+    paginator = Paginator(listings,6)
+    page=request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+
+    context={ 'listings' : paged_listings } 
+    return render(request, 'listings/listings.html' , context)
+
+def listing(request,listing_id ):
+    listing = get_object_or_404(Listing,pk=listing_id)
+
+    context = {
+        'listing' : listing
+    }
+    return render(request, 'listings/listing.html',context)
+
+def search(request):
+    query_list = Listing.objects.order_by('-list_date')
+    #keyword
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if 'keywords' :
+            query_list = query_list.filter(discriptional__icontains=keywords)   
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city :
+            query_list = query_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state :
+            query_list = query_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms :
+            query_list = query_list.filter(bedroom__lte=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price :
+            query_list = query_list.filter(price__lte=price)
+
+
+
+
+
+    
+
+
+
+    context={
+        'listings' : query_list,
+        'state_choices' : state_choices,
+        'bedroom_choices' : bedroom_choices,
+        'price_choices' : price_choices,
+        'values' : request.GET,
+    
+    }
+    return render(request, 'listings/search.html',context)
